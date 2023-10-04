2c4ecfe7a730f3a460712762e33a36fc9a7e2ab1
Alejandra Cardona <alejacgld@gmail.com>
2023-10-03 19:19:18 -0500

    A commit has been made

---
+++ Practica_Jenkins/src/main/java/com/mycompany/practica_jenkins/Practica_Jenkins.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_jenkins;
+
+/**
+ *
+ * @author aleja
+ */
+public class Practica_Jenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
