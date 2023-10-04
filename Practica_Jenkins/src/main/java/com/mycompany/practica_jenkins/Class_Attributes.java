31706c3cacc34183b7c1e56354f37791d5c6c86c
Alejandra Cardona <alejacgld@gmail.com>
2023-10-03 19:39:11 -0500

    Nueva clase y nuevos atributos

---
+++ Practica_Jenkins/src/main/java/com/mycompany/practica_jenkins/Class_Attributes.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica_jenkins;
+
+/**
+ *
+ * @author aleja
+ */
+public class Class_Attributes {
+    public String nombre;
+    public int celular;
+}
